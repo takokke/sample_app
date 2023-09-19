81f87535a36bd29a60b4ad2207a5b3b1d6c067b8
takokke <138221752+takokke@users.noreply.github.com>
2023-09-19 02:18:54 +0000

    投稿機能追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,30 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    if list.save
+      redirect_to '/top'
+    else
+      render "new"
+    end
   end
 
   def index
+    
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)  
   end
 end
