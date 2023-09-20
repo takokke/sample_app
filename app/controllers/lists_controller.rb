1706c745c3f0f03ae42f1dc839390e91bb2f8da2
takokke <138221752+takokke@users.noreply.github.com>
2023-09-20 13:31:18 +0000

    デバッグ機能追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,16 @@
 class ListsController < ApplicationController
-  
+
   def new
     @list = List.new
   end
-  
+
   def create
     # @を付けたインスタンス変数じゃないとrender先newの@listがnillになってしまう
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list.id), notice: '保存に成功しました'
+      redirect_to list_path(@list.id), notice: '投稿に成功しました'
     else
-      flash.now[alert] = '保存に失敗しました'
+      flash.now[:alert] = '投稿に失敗しました'
       render :new
     end
   end
@@ -24,28 +24,29 @@ class ListsController < ApplicationController
   end
 
   def edit
-    @list = List.find(params[:id])  
+    @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     if list.update(list_params)
-      redirect_to list_path(list.id), notice: '更新に成功しました'
+      redirect_to list_path(list.id) #, notice: '更新に成功しました'
     else
-      flash.now[alert] = '更新に失敗しました'
+      flash.now[:alert] = '更新に失敗しました'
       render :edit
+      
     end
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.delete
     redirect_to '/lists', notice: '投稿を削除しました'
   end
-  
+
   private
-  
+
   def list_params
-    params.require(:list).permit(:title, :body, :image)  
+    params.require(:list).permit(:title, :body, :image)
   end
 end
